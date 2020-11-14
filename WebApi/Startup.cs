abaae0bac7e8a43f62527fc325b6f08a86d4e0dc
曾衍仁 <zengyanren@foxmail.com>
2020-11-14 23:12:05 +0800

    添加swagger

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,11 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Powin Web API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,15 @@ namespace WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Powin Web API V1");
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -43,6 +58,8 @@ namespace WebApi
             {
                 endpoints.MapControllers();
             });
+
+            
         }
     }
 }
