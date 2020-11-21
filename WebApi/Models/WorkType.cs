820b91db1008d42141e724a8b3d4389e44b784b2
ray <ray@qq.com>
2020-11-21 14:51:00 +0800

    增加工作类型字段

---
+++ WebApi/Models/WorkType.cs
@@ -0,0 +1,17 @@
+﻿using FreeSql.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class WorkType : BaseEntity
+    {
+        [Column(DbType = "varchar(128) NOT NULL")]
+        public String Name { get; set; }
+        [Column(DbType = "varchar(128) NOT NULL")]
+        public String Description { get; set; }
+        public int ParentId { get; set; }
+    }
+}
